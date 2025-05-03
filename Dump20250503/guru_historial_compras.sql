ecccb4af54824cc4023bfc346141914ee54aa014
gato200308 <159741059+gato200308@users.noreply.github.com>
2025-05-03 12:04:43 -0500

    Add files via upload
    base de datos mejorada

---
+++ Dump20250503/guru_historial_compras.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: guru
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `historial_compras`
+--
+
+DROP TABLE IF EXISTS `historial_compras`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `historial_compras` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identificacion_id` varchar(20) DEFAULT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `total` decimal(10,2) DEFAULT NULL,
+  `productos` text,
+  PRIMARY KEY (`id`),
+  KEY `identificacion_id` (`identificacion_id`),
+  CONSTRAINT `historial_compras_ibfk_1` FOREIGN KEY (`identificacion_id`) REFERENCES `usuario` (`identificacion`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `historial_compras`
+--
+
+LOCK TABLES `historial_compras` WRITE;
+/*!40000 ALTER TABLE `historial_compras` DISABLE KEYS */;
+INSERT INTO `historial_compras` VALUES (1,'40219113','2025-05-03 18:57:46',80000.00,'[\"bolso\"]');
+/*!40000 ALTER TABLE `historial_compras` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-03 12:03:39
